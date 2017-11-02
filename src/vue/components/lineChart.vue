61f57d483fd0cda1d9627d21f4bfc01839cfafb8
benjamin <benjaminchen.c@gmail.com>
2017-11-02 17:53:27 +0800

    add line chart

---
+++ src/vue/components/lineChart.vue
@@ -0,0 +1,32 @@
+<template>
+    <md-card class="line-chart">
+        <md-card-media>
+            <line-chart
+                :data="data"
+                :options="options"
+                :width="width"
+                :height="height"
+            ></line-chart>
+        </md-card-media>
+    </md-card>
+</template>
+
+<script>
+    import Vue from 'vue';
+    import { Line } from 'vue-chartjs'
+
+    let LineChart = {
+        extends: Line,
+        props: ['data', 'options', 'height'],
+        mounted () {
+            this.renderChart(this.data, this.options)
+        }
+    }
+
+    export default {
+        components: {
+            LineChart
+        },
+        props: ['data', 'options', 'width', 'height'],
+    }
+</script>
