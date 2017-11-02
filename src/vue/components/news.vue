0a9c43a864c5f538ed02cc017a765ef44266531a
benjamin <benjaminchen.c@gmail.com>
2017-11-02 17:53:37 +0800

    add news

---
+++ src/vue/components/news.vue
@@ -0,0 +1,20 @@
+<template>
+    <md-card class="news">
+        <md-card-header>
+            <div class="md-title">新聞</div>
+        </md-card-header>
+        <md-card-media>
+            <md-list>
+                <md-list-item v-for="(row, index) in news" :key="index">
+                    <span>{{ row.title }}</span>
+                </md-list-item>
+            </md-list>
+        </md-card-media>
+    </md-card>
+</template>
+
+<script>
+    export default {
+        props: ['news'],
+    }
+</script>
