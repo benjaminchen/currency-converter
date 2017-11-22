8fdb49af0cf1457c2dc5279c393ca511e11389d4
benjamin <benjaminchen.c@gmail.com>
2017-11-22 12:16:45 +0800

    implement add, edit and delete function of card and row

--- src/vue/components/spendingCard.vue
+++ src/vue/components/spendingCard.vue
@@ -8,14 +8,30 @@
                         <md-button class="md-icon-button detail-btn" @click="toggle">
                             <md-icon>keyboard_arrow_down</md-icon>
                         </md-button>
-                        <md-button class="md-primary add-record-btn" @click="openDialog('add-dialog')">Add</md-button>
                     </md-layout>
                 </md-card-header-text>
+                <md-menu md-size="4" md-direction="bottom left">
+                    <md-button class="md-icon-button" md-menu-trigger>
+                        <md-icon>more_vert</md-icon>
+                    </md-button>
+
+                    <md-menu-content>
+                        <md-menu-item @click="openDialog('edit-row-dialog')">
+                        <span>修改標題</span>
+                        <md-icon>edit</md-icon>
+                        </md-menu-item>
+
+                        <md-menu-item @click="deleteRow">
+                        <span>刪除旅程</span>
+                        <md-icon>delete</md-icon>
+                        </md-menu-item>
+                    </md-menu-content>
+                </md-menu>
             </md-card-header>
             <md-card-content>
                 <md-list>
                     <md-list-item v-for="(expense, index) in expenses" class="md-inset" key="index">
-                        <md-layout md-flex="55">{{ expense.title}}</md-layout>
+                        <md-layout md-flex="50"><span>{{ expense.title}}</span></md-layout>
                         <md-layout md-flex="15">{{ expense.currency }}</md-layout>
                         <md-layout md-flex="20">{{ expense.money }}</md-layout>
                         <md-layout md-flex="10">
@@ -23,14 +39,44 @@
                                 <md-icon>edit</md-icon>
                             </md-button>
                         </md-layout>
+                        <md-layout md-flex="5">
+                            <md-button class="md-icon-button" @click="deleteExpense(index)">
+                                <md-icon>delete</md-icon>
+                            </md-button>
+                        </md-layout>
                     </md-list-item>
                     <md-list-item class="md-inset">
-                        <md-layout md-flex="55">總花費</md-layout>
+                        <md-layout md-flex="50">總花費</md-layout>
                         <md-layout md-flex="15">{{ currency }}</md-layout>
                         <md-layout md-flex="20">{{ sum }}</md-layout>
-                        <md-layout md-flex="10"></md-layout>
+                        <md-layout md-flex="15">
+                            <md-button class="md-icon-button" @click="openDialog('add-dialog')">
+                                <md-icon>add</md-icon>
+                            </md-button>
+                        </md-layout>
                     </md-list-item>
                 </md-list>
+
+                <md-dialog md-open-from="#fab" md-close-to="#fab" ref="edit-row-dialog">
+                    <md-dialog-title>修改旅程標題</md-dialog-title>
+
+                    <md-dialog-content>
+                        <form>
+                            <md-input-container>
+                                <input type="date" v-model="date">
+                            </md-input-container>
+                            <md-input-container>
+                                <label>標題</label>
+                                <md-textarea v-model="title"></md-textarea>
+                            </md-input-container>
+                        </form>
+                    </md-dialog-content>
+
+                    <md-dialog-actions>
+                        <md-button class="md-primary" @click="closeDialog('edit-row-dialog')">關閉</md-button>
+                    </md-dialog-actions>
+                </md-dialog>
+
                 <md-dialog md-open-from="#fab" md-close-to="#fab" ref="add-dialog">
                     <md-dialog-title>新增支出</md-dialog-title>
 
@@ -56,6 +102,7 @@
                         <md-button class="md-primary" @click="editOrAdd('add-dialog')">新增</md-button>
                     </md-dialog-actions>
                 </md-dialog>
+
                 <md-dialog md-open-from="#fab" md-close-to="#fab" ref="edit-dialog">
                     <md-dialog-title>更新支出</md-dialog-title>
 
@@ -92,7 +139,9 @@
             'date',
             'title',
             'expenses',
-            'currency'
+            'currency',
+            'deleteRow',
+            'saveRows'
         ],
         data: () => ({
                 editIndex: null,
@@ -131,6 +180,9 @@
                 index ? this.editExpense = nullExpense : this.expenses.push(this.newExpense)
                 this.newExpense = nullExpense
                 this.closeDialog(ref)
+            },
+            deleteExpense(index) {
+                this.expenses.splice(index, 1)
             }
         },
         computed: {
@@ -141,6 +193,11 @@
                     var money = this.currency === expense.currency ? (total + m) : (total + m * rate)
                     return Math.round(money * 100) / 100
                 }, 0)
+            }
+        },
+        watch: {
+            'expenses'(expenses) {
+                this.saveRows()
             }
         }
     }
--- src/vue/record.vue
+++ src/vue/record.vue
@@ -1,7 +1,27 @@
 <template>
     <div>
-        <card v-for="(row, index) in rows" :date="row.date" :title="row.title" :expenses="row.expenses" :currency="currency" key="index"></card>
-        <md-button class="md-icon-button md-raised md-accent add-btn">
+        <card v-for="(row, index) in rows" :date="row.date" :title="row.title" :expenses="row.expenses" :currency="currency" :deleteRow="deleteRow" :saveRows="saveRows" key="index"></card>
+        <md-dialog md-open-from="#fab" md-close-to="#fab" ref="add-dialog">
+            <md-dialog-title>新增旅程紀錄</md-dialog-title>
+
+            <md-dialog-content>
+                <form>
+                    <md-input-container>
+                        <input type="date" v-model="newRow.date">
+                    </md-input-container>
+                    <md-input-container>
+                        <label>標題</label>
+                        <md-textarea v-model="newRow.title"></md-textarea>
+                    </md-input-container>
+                </form>
+            </md-dialog-content>
+
+            <md-dialog-actions>
+                <md-button class="md-primary" @click="closeDialog('add-dialog')">取消</md-button>
+                <md-button class="md-primary" @click="editOrAdd('add-dialog')">新增</md-button>
+            </md-dialog-actions>
+        </md-dialog>
+        <md-button class="md-icon-button md-raised md-accent add-row-btn" @click="openDialog('add-dialog')">
             <md-icon>add</md-icon>
         </md-button>
     </div>
@@ -11,6 +31,10 @@
     import card from './components/spendingCard.vue'
 
     export default {
+        mounted: function() {
+            var rows = window.localStorage.getItem("TravelToolRows");
+            if (rows) this.rows = JSON.parse(rows)
+        },
         props: [
             'currency'
         ],
@@ -19,10 +43,21 @@
         },
         data: function() {
             return {
+                newRow: {
+                    date: "",
+                    title: "",
+                    expenses: []
+                },
+                editIndex: null,
+                editRow: {
+                    date: "",
+                    title: "",
+                    expenses: []
+                },
                 rows: [
                     {
-                        date: "2017/10/20",
-                        title: "日本-範例",
+                        date: "2017-10-20",
+                        title: "日本花費範例",
                         expenses: [
                             {
                                 title: "機票",
@@ -40,6 +75,40 @@
             }
         },
         methods: {
+            openDialog(ref, row = null, index = null) {
+                if (row) {
+                    this.editRow = row
+                    this.editIndex = index
+                }
+                this.$refs[ref].open()
+            },
+            closeDialog(ref) {
+                this.$refs[ref].close()
+            },
+            editOrAdd(ref, index = null) {
+                var nullRow = {
+                    date: "",
+                    title: "",
+                    expenses: []
+                }
+                index ? this.editRow = nullRow : this.rows.push(this.newRow)
+                this.newRow = nullRow
+                this.closeDialog(ref)
+                this.rows.sort(function(a, b) {
+                    return new Date(a.date) < new Date(b.date)
+                })
+            },
+            deleteRow(e) {
+                this.rows.splice(e, 1)
+            },
+            saveRows() {
+                window.localStorage.setItem("TravelToolRows", JSON.stringify(this.rows))
+            }
+        },
+        watch: {
+            'rows'(rows) {
+                this.saveRows()
+            }
         }
     }
 </script>
